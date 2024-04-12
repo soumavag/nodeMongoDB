62ca60311f65526ccd1441bb2f5aa7af6ae6b8a6
soumavag <soumava.g@outlook.com>
2024-04-12 10:14:44 -0500

    commit on index.js

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-require("dotenv").config();
-console.log(process.env.ENVIRONMENT);
+config = require("dotenv").config();
+const logger = require('./logger');
+
+console.log(process.env.CURR_ENVIRONMENT);
+
+logger.log("A message")
+
+
+
+
